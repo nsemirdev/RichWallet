f31eedfe26fb91efb53b14c43c4aad9552ac0ae0
nsemirdev <nsemirdev@gmail.com>
2023-01-23 16:34:12 +0300

    initial project

---
+++ RichWallet/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RichWallet
+//
+//  Created by Emir Alkal on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemTeal
+  }
+}
